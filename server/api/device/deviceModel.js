7c1e7ff105baf4be4fdc407438e09aa25aba3b8b
Riste Poposki <rickyarpy@hotmail.com>
2019-01-19 01:57:06 +0100

    added isActive and isAddedToDashboard to Device Model

--- server/api/device/deviceModel.js
+++ server/api/device/deviceModel.js
@@ -7,17 +7,19 @@ var DeviceSchema = new Schema({
         unique: true,
         required: true
     },
-    password:{
-        type:String,
-        select : false
+    password: {
+        type: String,
+        select: false
     },
     description: String,
     location: {
         lat: String,
         lng: String,
-        accuracy:String,
+        accuracy: String,
         description: String
-    }
+    },
+    isActive: { type: Boolean, default: false },
+    isAddedToDashboard: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('device', DeviceSchema);
