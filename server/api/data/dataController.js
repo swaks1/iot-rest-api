908f0b4e4ec2a4301a7b396f02be558edc543a4a
Riste Poposki <rickyarpy@hotmail.com>
2019-01-24 21:35:06 +0100

    data filtering with query strings: period and pageSize

--- server/api/data/dataController.js
+++ server/api/data/dataController.js
@@ -19,18 +19,113 @@ controller.param = (req, res, next, id) => {
 
 controller.get = (req, res, next) => {
     let deviceId = req.query.deviceId;
-    //logger.log(deviceId);
+    let period = req.query.period;
+    let pageSize = 10;
+
+    //try get pageSize
+    try {
+        if (req.query.pageSize)
+            pageSize = parseInt(req.query.pageSize);
+    } catch (error) {
+        logger.error(error);
+    }
+
     if (!deviceId) {
         res.send("add device id");
     } else {
-        Data.find({ device: deviceId })
-            .sort({ created: 'asc' })
-            //.populate('device')
-            .exec()
-            .then(data => {
-                res.json(data);
-            },
-                err => next(err));
+        switch (period) {
+            case "daily":
+                //return ALL
+                Data.aggregate([
+                    {
+                        $addFields: {
+                            dataValueDecimal: { $convert: { input: "$dataItem.dataValue", to: "decimal", onError: "Error", onNull: 0 } },
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: { year: { $year: "$created" }, month: { $month: "$created" }, day: { $dayOfMonth: "$created" } },
+                            average: { $avg: "$dataValueDecimal" },
+                            dataType: { $first: "$dataItem.dataType" }
+                        }
+                    },
+                    {
+                        $sort: {
+                            '_id.year': -1,
+                            '_id.month': -1,
+                            '_id.day': -1
+                        }
+                    },
+                    {
+                        $limit: pageSize,
+                    }
+                ],
+                    function (error, data) {
+                        if (error) {
+                            next(error);
+                        }
+                        else {
+                            res.json(data);
+                        }
+
+                    });
+                break;
+            case "lastHour":
+                //return ALL
+                Data.aggregate([
+                    {
+                        $match: {
+                            'created': { $gt: new Date(Date.now() - 1000 * 60 * 60) } //only those in last hour
+                        }
+                    },
+                    {
+                        $addFields: {
+                            dataValueDecimal: { $convert: { input: "$dataItem.dataValue", to: "decimal", onError: "Error", onNull: 0 } },
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: { year: { $year: "$created" }, month: { $month: "$created" }, day: { $dayOfMonth: "$created" }, hour: { $hour: "$created" }, minute: { $minute: "$created" } },
+                            average: { $avg: "$dataValueDecimal" },
+                            dataType: { $first: "$dataItem.dataType" }
+                        }
+                    },
+                    {
+                        $sort: {
+                            '_id.year': -1,
+                            '_id.month': -1,
+                            '_id.day': -1,
+                            '_id.hour': - 1,
+                            '_id.minute': -1
+                        }
+                    },
+                    {
+                        $limit: pageSize,
+                    }
+                ],
+                    function (error, data) {
+                        if (error) {
+                            next(error);
+                        }
+                        else {
+                            res.json(data);
+                        }
+
+                    });
+                break;
+            default:
+                //return ALL
+                Data.find({ device: deviceId })
+                    .sort({ created: 'desc' })
+                    .limit(pageSize)
+                    .exec()
+                    .then(data => {
+                        res.json(data);
+                    },
+                        err => next(err));
+
+        }
+
     }
 };
 
