26054b3e34030166b87b254dd883a309ceeba0e2
Riste Poposki <rickyarpy@hotmail.com>
2019-01-25 01:41:01 +0100

    mapping of the data in correct format... added monthly data period

--- server/api/data/dataController.js
+++ server/api/data/dataController.js
@@ -33,9 +33,72 @@ controller.get = (req, res, next) => {
     if (!deviceId) {
         res.send("add device id");
     } else {
+        let currentYear = new Date().getFullYear();
+
         switch (period) {
+            case "monthly":
+                //return grouped by month for current year
+                Data.aggregate([
+                    {
+                        $match: {
+                            'created': { $gt: new Date(`${currentYear}-01-01T00:00:00.000Z`) } //only those in last hour
+                        }
+                    },
+                    {
+                        $addFields: {
+                            dataValueDecimal: { $convert: { input: "$dataItem.dataValue", to: "decimal", onError: "Error", onNull: 0 } },
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: { month: { $month: "$created" } },
+                            average: { $avg: "$dataValueDecimal" },
+                            dataType: { $first: "$dataItem.dataType" }
+                        }
+                    },
+                    {
+                        $sort: {
+                            '_id.month': 1,
+                        }
+                    },
+                    {
+                        $limit: pageSize,
+                    }
+                ],
+                    function (error, data) {
+                        if (error) {
+                            next(error);
+                        }
+                        else {
+                            var mappedData = data.map((item, index) => {
+
+                                let obj = JSON.parse(JSON.stringify(item.average).replace("$", "")); //the average is object that canot be accesed....
+                                let average = obj.numberDecimal;
+
+                                let year = currentYear;
+                                let month = item._id.month;
+
+                                if (month.toString().length == 1) {
+                                    month = "0" + month;
+                                }
+
+                                return {
+                                    dataItem: {
+                                        dataValue: average,
+                                        dataType: item.dataType
+                                    },
+                                    _id: `data${index}`,
+                                    device: deviceId,
+                                    created: `${year}-${month}`
+                                };
+                            });
+                            res.json(mappedData);
+                        }
+
+                    });
+                break;
             case "daily":
-                //return ALL
+                //return grouped by day
                 Data.aggregate([
                     {
                         $addFields: {
@@ -65,13 +128,39 @@ controller.get = (req, res, next) => {
                             next(error);
                         }
                         else {
-                            res.json(data);
+                            var mappedData = data.map((item, index) => {
+
+                                let obj = JSON.parse(JSON.stringify(item.average).replace("$", "")); //the average is object that canot be accesed....
+                                let average = obj.numberDecimal;
+
+                                let year = item._id.year;
+                                let month = item._id.month;
+                                let day = item._id.day;
+
+                                if (month.toString().length == 1) {
+                                    month = "0" + month;
+                                }
+
+                                if (day.toString().length == 1) {
+                                    day = "0" + day;
+                                }
+                                return {
+                                    dataItem: {
+                                        dataValue: average,
+                                        dataType: item.dataType
+                                    },
+                                    _id: `data${index}`,
+                                    device: deviceId,
+                                    created: `${year}-${month}-${day}`
+                                };
+                            });
+                            res.json(mappedData);
                         }
 
                     });
                 break;
             case "lastHour":
-                //return ALL
+                //return only those in last hour grouped by minute
                 Data.aggregate([
                     {
                         $match: {
@@ -108,13 +197,47 @@ controller.get = (req, res, next) => {
                             next(error);
                         }
                         else {
-                            res.json(data);
+                            var mappedData = data.map((item, index) => {
+
+                                let obj = JSON.parse(JSON.stringify(item.average).replace("$", "")); //the average is object that canot be accesed....
+                                let average = obj.numberDecimal;
+
+                                let year = item._id.year;
+                                let month = item._id.month;
+                                let day = item._id.day;
+                                let hour = item._id.hour;
+                                let minute = item._id.minute;
+
+                                if (month.toString().length == 1) {
+                                    month = "0" + month;
+                                }
+                                if (day.toString().length == 1) {
+                                    day = "0" + day;
+                                }
+                                if (hour.toString().length == 1) {
+                                    hour = "0" + hour;
+                                }
+                                if (minute.toString().length == 1) {
+                                    minute = "0" + minute;
+                                }
+
+                                return {
+                                    dataItem: {
+                                        dataValue: average,
+                                        dataType: item.dataType
+                                    },
+                                    _id: `data${index}`,
+                                    device: deviceId,
+                                    created: `${year}-${month}-${day}T${hour}:${minute}`
+                                };
+                            });
+                            res.json(mappedData);
                         }
 
                     });
                 break;
-            default:
-                //return ALL
+            default: //or period=mostRecent
+                //return most recent data
                 Data.find({ device: deviceId })
                     .sort({ created: 'desc' })
                     .limit(pageSize)
