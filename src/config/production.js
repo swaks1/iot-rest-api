c1870200c764d7bdc05dab172d50a27223e93a4c
Riste Poposki <rickyarpy@hotmail.com>
2019-11-13 23:48:51 +0100

    config merged

---
+++ src/config/production.js
@@ -0,0 +1,7 @@
+// this config will be merged with the default one found in index.js
+// if the enviroment was set to produciton
+
+export const config = {
+  logging: false,
+  dbUrl: "mongodb://localhost:27017/iotApp-prod"
+};
